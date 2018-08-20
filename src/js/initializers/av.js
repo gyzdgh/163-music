8a41b6fff8935915f012a56cc2b7b0effc472d73
高远 <gyz@gaozhandeMacBook-Pro.local>
2018-08-20 17:32:26 +0800

    添加模块化 && 引入命名空间

---
+++ src/js/initializers/av.js
@@ -0,0 +1,19 @@
+var APP_ID = 'OLE1sN9FMRyCmNuMhfxw9wbC-gzGzoHsz';
+var APP_KEY = 'usxMwMoSm8tW69MI33NNhWJn';
+
+AV.init({
+    appId: APP_ID,
+    appKey: APP_KEY
+});
+
+// var TestObject = AV.Object.extend('Playlist');
+// var testObject = new TestObject();
+// testObject.save({
+//     name: 'test',
+//     cover: 'test',
+//     creatorId: 'test',
+//     description: 'test',
+//     songs: ['1', '2']
+// }).then(function (object) {
+//     alert('LeanCloud Rocks!');
+// })
