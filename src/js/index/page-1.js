8b572848d718116adaf356f4a12fb2211ffd0c5f
高远 <gyz@gaozhandeMacBook-Pro.local>
2018-08-23 17:04:00 +0800

    添加 page1 最新音乐 模块

--- src/js/index/page-1.js
+++ src/js/index/page-1.js
@@ -18,6 +18,8 @@
             this.model = model
             this.view.init()
             this.bindEventHub()
+            this.loadModule1()
+            this.loadModule2()
         },
         bindEventHub(){
             window.eventHub.on('selectTab',(tabName)=>{
@@ -27,6 +29,24 @@
                     this.view.hide()
                 }
             })
+        },
+        //加载模块1
+        loadModule1(){
+            let script1 = document.createElement('script')
+            script1.src = './js/index/page-1-1.js'
+            script1.onload = function(){
+                console.log('1 over')
+            }
+            document.body.appendChild(script1)
+        },
+        //加载模块2
+        loadModule2(){
+            let script2 = document.createElement('script')
+            script2.src = './js/index/page-1-2.js'
+            script2.onload = function(){
+                console.log('2 over')
+            }
+            document.body.appendChild(script2)
         }
     }
     controller.init(view, model)
