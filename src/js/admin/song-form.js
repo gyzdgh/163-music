73a7b17ebfd487292ea40d5926811bf630b2b8a0
高远 <gyz@gaozhandeMacBook-Pro.local>
2018-08-24 17:19:41 +0800

    播放时动画

--- src/js/admin/song-form.js
+++ src/js/admin/song-form.js
@@ -25,13 +25,19 @@
                         </label>
                         <input name="url" type="text" value="__url__">
                     </div>
+                    <div class="row">
+                        <label>
+                            封面
+                        </label>
+                        <input name="cover" type="text" value="__cover__">
+                    </div>
                     <div class="row actions">
                         <button type="submit">保存</button>
                     </div>
                 </form>
                 `,
         render(data = {}) {
-            let placeholders = ['name', 'url', 'singer', 'id']
+            let placeholders = ['name', 'url', 'singer', 'id', 'cover']
             let html = this.template
             placeholders.map((string) => {
                 html = html.replace(`__${string}__`, data[string] || '')
@@ -49,7 +55,7 @@
     }
     let model = {
         data: {
-            name: '', singer: '', url: '', id: ''
+            name: '', singer: '', url: '', id: '', cover:''
         },
         //上传数据
         update(data) { 
@@ -57,6 +63,7 @@
             song.set('name', data.name)
             song.set('singer', data.singer)
             song.set('url', data.url)
+            song.set('cover', data.cover)
             return song.save().then((response) => {
                 Object.assign(this.data, data)
                 return response
@@ -69,6 +76,7 @@
             song.set('name', data.name);
             song.set('singer', data.singer);
             song.set('url', data.url);
+            song.set('cover', data.cover)
             return song.save().then((newSong) => {
                 let { id, attributes } = newSong
                 Object.assign(this.data, { id, ...attributes })
@@ -103,7 +111,7 @@
         },
         //创建数据
         create() {
-            let needs = 'name singer url'.split(' ')
+            let needs = 'name singer url cover'.split(' ')
             let data = {}
             needs.map((string) => {
                 data[string] = this.view.$el.find(`[name="${string}"]`).val()
@@ -118,7 +126,7 @@
         },
         //上传数据
         update() {
-            let needs = 'name singer url'.split(' ')
+            let needs = 'name singer url cover'.split(' ')
             let data = {}
             needs.map((string) => {
                 data[string] = this.view.$el.find(`[name="${string}"]`).val()
--- src/js/song/app.js
+++ src/js/song/app.js
@@ -1,76 +1,84 @@
 {
     let view = {
         el: '#app',
-        template: `
-            <audio src={{url}}></audio>
-            <div>
-                <button class="play">播放</button>
-                <button class="pause">暂停</button>
-            </div>
-        `,
         render(data){
-            $(this.el).html(this.template.replace('{{url}}', data.url))
+            let {song, status} = data
+            $(this.el).css('background-image', `url(${song.cover})`)
+            $(this.el).find('img.cover').attr('src', song.cover)
+            if($(this.el).find('audio').attr('src') !== song.url){
+                $(this.el).find('audio').attr('src', song.url)
+            }
+            if(status === 'playing'){
+                $(this.el).find('.disc-container').addClass('playing')
+            }else{
+                $(this.el).find('.disc-container').removeClass('playing')
+            }
         },
         play(){
-            let audio = $(this.el).find('audio')[0]
-            audio.play()
+            $(this.el).find('audio')[0].play()
         },
         pause(){
-            let audio = $(this.el).find('audio')[0]
-            audio.pause()
+            $(this.el).find('audio')[0].pause()
         }
     }
     let model = {
-        data:{
-            id: '',
-            name: '',
-            singer: '',
-            url: ''
+        data: {
+            song: {
+                id: '',
+                name: '',
+                singer: '',
+                url: ''
+            },
+            status: 'paused'
         },
         //根据 ID 获取歌曲信息
-        get(id){
+        get(id) {
             var query = new AV.Query('Song')
-            return query.get(id).then((song)=>{
-                Object.assign(this.data, {id: song.id, ...song.attributes})
+            return query.get(id).then((song) => {
+                Object.assign(this.data.song, { id: song.id, ...song.attributes })
                 return song
             })
         }
     }
     let controller = {
-        init(view, model){
+        init(view, model) {
             this.view = view
             this.model = model
             let id = this.getSongId()
-            this.model.get(id).then(()=>{
+            this.model.get(id).then(() => {
                 this.view.render(this.model.data)
             })
             this.bindEvents()
         },
         //播放 暂停
-        bindEvents(){
-            $(this.view.el).on('click', '.play',()=>{
+        bindEvents() {
+            $(this.view.el).on('click', '.icon-play', ()=>{
+                this.model.data.status = 'playing'
+                this.view.render(this.model.data)
                 this.view.play()
             })
-            $(this.view.el).on('click', '.pause', ()=>{
+            $(this.view.el).on('click', '.icon-pause', ()=>{
+                this.model.data.status = 'paused'
+                this.view.render(this.model.data)
                 this.view.pause()
             })
         },
         //获取歌曲 id
-        getSongId(){
+        getSongId() {
             let search = window.location.search
             //过滤字符串中的 ？ 
-            if(search.indexOf('?') === 0){
+            if (search.indexOf('?') === 0) {
                 search = search.substring(1)
             }
 
             //过滤字符串中的 & 
-            let array = search.split('&').filter((v=>v))
+            let array = search.split('&').filter((v => v))
             let id = ''
-            for(let i=0;i<array.length;i++){
+            for (let i = 0; i < array.length; i++) {
                 let kv = array[i].split('=')
                 let key = kv[0]
                 let value = kv[1]
-                if(key === 'id'){
+                if (key === 'id') {
                     id = value
                     break
                 }
